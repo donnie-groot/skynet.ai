3230c51e3c3ec689e16c65b2e8c4551e243d4321
donnie-groot <donniegroot@gmail.com>
2026-06-12 01:29:08 +0000

    feat: use argparse to accept user prompt from command line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ###### standard library imports ######
 import os 
+import argparse
 ###### 3rd party imports ######
 from dotenv import load_dotenv
 from google import genai
@@ -7,32 +8,12 @@ from google import genai
 ###### local imports ######
 # from module import something
 
-# grabing api key
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-# error handleing in case of no key
-
-if api_key == None:
-    raise RuntimeError("no api key found please imput api key")
-
-# seting up client so we can make requests 
-client = genai.Client(api_key=api_key)
-
-# getting our output
-response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-
-
-if response.usage_metadata is None:
-    raise RuntimeError("usage_metadata is None, API request may have failed")
-
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
-print(response.text)
-
-
 
 def main():
+    # setting up parser 
+    parser = argparse.ArgumentParser(description="skynet")
+    parser.add_argument("user_prompt", type=str, help="user_prompt")
+    args = parser.parse_args()
     # grabing api key
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -46,11 +27,16 @@ def main():
     client = genai.Client(api_key=api_key)
 
     # getting our output
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
 
 
     if response.usage_metadata is None:
         raise RuntimeError("usage_metadata is None, API request may have failed")
+
+
+    parser = argparse.ArgumentParser(description="skynet")
+    parser.add_argument("user_promt", type=str, help="user prompt")
+    args = parser.parse_args
 
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
