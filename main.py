07667e5d9cb3b9cc5f31d4a6df640b27ab283eef
donnie-groot <donniegroot@gmail.com>
2026-06-12 01:11:19 +0000

    feat: added token promt and useage to output

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 ###### standard library imports ######
 import os 
-
 ###### 3rd party imports ######
 from dotenv import load_dotenv
 from google import genai
@@ -21,14 +20,41 @@ if api_key == None:
 client = genai.Client(api_key=api_key)
 
 # getting our output
-output = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
 
-print(output.text)
+
+if response.usage_metadata is None:
+    raise RuntimeError("usage_metadata is None, API request may have failed")
+
+print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+print("Response tokens:", response.usage_metadata.candidates_token_count)
+print(response.text)
 
 
 
 def main():
-    print("Hello from skynet-ai!")
+    # grabing api key
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    # error handleing in case of no key
+
+    if api_key == None:
+        raise RuntimeError("no api key found please imput api key")
+
+    # seting up client so we can make requests 
+    client = genai.Client(api_key=api_key)
+
+    # getting our output
+    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+
+
+    if response.usage_metadata is None:
+        raise RuntimeError("usage_metadata is None, API request may have failed")
+
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print(response.text)
 
 
 if __name__ == "__main__":
